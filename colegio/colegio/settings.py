1102607abc55a443924dbf6313e704bbd4752f32
agusfranco706 <agustinfranco20191@gmail.com>
2024-08-28 00:35:26 -0300

    quinto commit

--- colegio/colegio/settings.py
+++ colegio/colegio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%wx#j_it5w(k0)*@)z@wgqp(#l^)a9vuyc=p$y@ec9qnq&g3+-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- colegio/registro/urls.py
+++ colegio/registro/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 path('alta_docente/', views.alta_docente, name='alta_docente'),
 path('buscar_docente/', views.buscar_docente, name='buscar_docente'),
-path('lista_docentes/', views.lista_docentes, name='lista_docentes')
+path('lista_docentes/', views.lista_docentes, name='lista_docentes'),
 ]
